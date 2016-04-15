f6fbabfce1288f6ea955d6e28990d10c0e8b6198
tim-kretschmer-c2fo <tim.kretschmer@c2fo.com>
2016-04-15 10:00:48 -0500

    DVO-5723 - etcd query script

---
+++ script/etcd/get-all.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# Reference: https://github.com/coreos/etcd/tree/master/etcdctl
+
+export ETCDCTL_PEERS=${ETCDCRL_PEERS:-"http://localhost:4002"}
+export ETCDCTL_ENDPOINT=${ETCDCTL_ENDPOINT:-"http://localhost:4002"}
+
+etcdctl --no-sync ls --recursive -p | while read -r line ; do
+    if [[ "${line:${#line}-1}" != "/" ]]; then
+    	stored=$(etcdctl --no-sync get $line)
+    	echo "$line -> $stored"
+    fi
+done
